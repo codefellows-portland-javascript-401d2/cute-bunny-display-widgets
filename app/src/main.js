ac2fa12b26d696ee340f7989dd4602ff4fc5b801
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-23 00:41:56 -0700

    Removed ng material, created image responsive class

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,6 @@
-import 'angular-material/angular-material.css';
 import './styles/main.css';
 
 import ng from 'angular';
-import ngMaterial from 'angular-material';
 import app from './bunny-app';
 
-ng.bootstrap(document, [app, ngMaterial]);
+ng.bootstrap(document, [app]);
