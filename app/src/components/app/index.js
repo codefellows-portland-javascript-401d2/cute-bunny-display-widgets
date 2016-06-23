9525771350cce6a3e8d90e494a9a4bc14cb244ad
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-22 21:53:56 -0700

    Created bunny full component

--- app/src/components/app/index.js
+++ app/src/components/app/index.js
@@ -7,7 +7,15 @@ export default {
 		this.data = {
 			title: 'Calico Bunny',
 			description: 'Too cute to be real.',
-			link: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
+			link: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
+			thumb: {
+				width: 100,
+				height: 67
+			},
+			full: {
+				width: 2896,
+				height: 1936
+			}
 		};
 	}
 };
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -2,11 +2,13 @@ import ng from 'angular';
 import app from './app';
 import bunnyText from './bunny-text';
 import bunnyThumb from './bunny-thumb';
+import bunnyFull from './bunny-full';
 
 const components = ng
   .module('components', [])
 	.component('app', app)
   .component('bunnyText', bunnyText)
   .component('bunnyThumb', bunnyThumb)
+  .component('bunnyFull', bunnyFull)
 
 export default components.name;
