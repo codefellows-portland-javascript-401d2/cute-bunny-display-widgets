ced682891a502ad187026a88a7913116946b1c63
Johnny Luangphasy <contact@jluangphasy.com>
2016-06-22 21:46:08 -0700

    Created bunny thumb component

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,10 +1,12 @@
 import ng from 'angular';
 import app from './app';
 import bunnyText from './bunny-text';
+import bunnyThumb from './bunny-thumb';
 
 const components = ng
   .module('components', [])
 	.component('app', app)
   .component('bunnyText', bunnyText)
+  .component('bunnyThumb', bunnyThumb)
 
 export default components.name;
