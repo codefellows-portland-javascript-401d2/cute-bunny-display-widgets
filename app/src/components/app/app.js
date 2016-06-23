62c15c6704e96c1486c46532e90b8e0513ba7cbd
Johnny Luangphasy <contact@jluangphasy.com>
2016-06-22 21:21:29 -0700

    Created bunny text component

--- app/src/components/app/app.js
+++
@@ -1,7 +0,0 @@
-import template from './app.html';
-
-export default {
-	template,
-	controllerAs: 'app',
-	controller: () => {}
-};
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,8 +1,10 @@
 import ng from 'angular';
-import app from './app/app';
+import app from './app';
+import bunnyText from './bunny-text';
 
 const components = ng
   .module('components', [])
 	.component('app', app)
+  .component('bunnyText', bunnyText)
 
 export default components.name;
