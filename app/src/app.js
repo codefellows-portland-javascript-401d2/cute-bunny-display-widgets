a223507ee5ed9b85dcd418f9026fd0e83aabef78
DonChatelain <djchatelain@gmail.com>
2016-06-24 16:36:45 -0700

    nicely styles radio buttons as tabs, adds more monsters,

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import components from './components';
+import ngAnimate from 'angular-animate';
 
-const app = angular.module('displayApp', [components]);
+const app = angular.module('displayApp', [components, ngAnimate]);
 
 export default app.name;
