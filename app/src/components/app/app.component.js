9e91a77f7283d86037f89b5e60e8a49317fae7df
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-24 15:46:54 -0700

    Added styles for app component

--- app/src/components/app/app.component.js
+++ app/src/components/app/app.component.js
@@ -1,9 +1,11 @@
 import template from './app.view.html';
+import styles from './app.styles.scss';
 import bunnies from '../../data/bunnies.json';
 
 export default {
 	template,
 	controller() {
+		this.styles = styles;
 		this.currentSection = 'list';
 		this.bunnies = bunnies;
 	}
