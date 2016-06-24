ba4c8e0c2f0f948b7b7d69069427696b0c4159d2
Johnny Luangphasy <johnny@jluangphasy.com>
2016-06-24 09:17:10 -0700

    Photo gallery working on full view

--- app/src/components/album/album.module.js
+++ app/src/components/album/album.module.js
@@ -1,6 +1,6 @@
 import ng from 'angular';
 import appComponent from '../app/app.component';
-import albumNavComponent from './album-nav/album-nav.component';
+import albumHeadComponent from './album-head/album-head.component';
 import albumListComponent from './album-list/album-list.component';
 import albumTileComponent from './album-tile/album-tile.component';
 import albumFullComponent from './album-full/album-full.component';
@@ -8,7 +8,7 @@ import albumFullComponent from './album-full/album-full.component';
 const albumModule = ng
   .module('components', [])
 	.component('app', appComponent)
-  .component('albumNav', albumNavComponent)
+  .component('albumHead', albumHeadComponent)
   .component('albumList', albumListComponent)
   .component('albumTile', albumTileComponent)
   .component('albumFull', albumFullComponent);
