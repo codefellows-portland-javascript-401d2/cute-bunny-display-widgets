3943e87274ba3c271f37a74095d89dc091504974
Johnny Luangphasy <contact@jluangphasy.com>
2016-06-23 21:08:06 -0700

    Set layout for all displays using pills

--- app/src/components/album/album-tile/album-tile.component.js
+++ app/src/components/album/album-tile/album-tile.component.js
@@ -7,5 +7,20 @@ export default {
   },
   require: {
     app: '^^'
+  },
+  controller() {
+    let bunnies = [];
+    let bunniesGroup = [];
+
+    for (let i = 0; i < this.bunnies.length; i++) {
+      bunnies.push(this.bunnies[i]);
+
+      if (i % 3 === 2) {
+        bunniesGroup.push(bunnies);
+        bunnies = [];
+      }
+    }
+
+    this.bunniesGroup = bunniesGroup;
   }
 };
