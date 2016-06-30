f2827963dac9b7925c9b119e7aba15df9f67c025
Johnny Luangphasy <contact@jluangphasy.com>
2016-06-30 09:43:18 -0700

    App component test working

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,6 @@ import './styles/main.scss';
 import ng from 'angular';
 import appModule from './components/app/app.module';
 
-appModule.value('apiUrl', process.env.API_URL);
+appModule.value('apiUrl', 'http://localhost:3000/api');
 
 ng.bootstrap(document, [appModule.name]);
--- app/test/app.component.test.js
+++ app/test/app.component.test.js
@@ -1,19 +1,38 @@
-// describe('app component', () => {
-// 	let $component;
-// 	let appComponent;
-//
-// 	beforeEach(angular.mock.module('app'));
-//
-// 	beforeEach(angular.mock.inject($injector => {
-// 		$component = $injector.get('$componentController');
-// 	}));
-//
-// 	it('intializes with correct data', () => {
-// 		appComponent = $component('app');
-//
-// 		expect(Object.keys(appComponent.styles).sort()).toEqual(['header', 'main']);
-// 		expect(appComponent.currentSection).toEqual('list');
-//     expect(appComponent.bunnies).toEqual([]);
-//     expect(appComponent.bunniesLoaded).toBeFalsy();
-// 	});
-// });
+describe('app component', () => {
+	let $component;
+	let appComponent;
+  let albumsSvc = {};
+  let albumId = 1;
+
+  beforeEach(angular.mock.module('services', $provide => {
+    $provide.value('apiUrl', '/api');
+  }));
+
+  beforeEach(angular.mock.module('app'));
+
+	// beforeEach(angular.mock.module('app', {
+  //   albumsService: albumsSvc
+  // }));
+
+	beforeEach(angular.mock.inject($injector => {
+		$component = $injector.get('$componentController');
+	}));
+
+	it('intializes with correct data', () => {
+		appComponent = $component('app');
+
+		expect(Object.keys(appComponent.styles).sort()).toEqual(['header', 'main']);
+		expect(appComponent.currentSection).toEqual('list');
+    expect(appComponent.bunnies).toEqual([]);
+    expect(appComponent.bunniesLoaded).toBeFalsy();
+
+    // albumsSvc.getAlbums = () => {
+		// 	return Promise.resolve([]);
+		// };
+    //
+    // albumsSvc.getImages = (id) => {
+    //   expect(id).toEqual(albumId);
+    //   return Promise.resolve([]);
+    // };
+	});
+});
